dbc1836b9aff1298ef7bfe7067a1e22596ead68c
Anuja Prakash Kolse <anujaprakashkolse@Anujas-MBP.cable.rcn.com>
2023-06-25 11:08:13 -0400

    fixed tab issue

--- src/zinema/settings/security.js
+++ src/zinema/settings/security.js
@@ -5,65 +5,93 @@ import { useSelector, useDispatch } from "react-redux";
 import React, { useState, useEffect } from "react";
 
 function Security() {
-    const { currentUser } = useSelector((state) => state.user);
-    const [profile, setProfile] = useState(currentUser);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const save = () => { dispatch(updateUserThunk(profile)); };
-    useEffect(() => {
-        async function fetchData() {	
-            const { payload } = await dispatch(profileThunk());	
-            setProfile(payload);	
-          }	
-          fetchData();	
-        }, [dispatch]);
-    return (
-        <div className="container-xl px-4 mt-4">
-            <div>
-                <nav className="nav nav-tabs mb-2">
-                    <Link className="nav-link ms-0" to="/zinema/settings-main">Profile</Link>
-                    <Link className="nav-link" to="/zinema/billing">Billing</Link>
-                    <Link className="nav-link active" to="/zinema/security">Security</Link>
-                </nav>
-            <div style={{color: "black"}}>
-                <h1>Security Page</h1>
-                
-                <div className="row">
-                    <div className="col-4">
-                        <label>Change Username</label>
-                    </div>
-                    <div className="col-8">
-                        <input className="form-control" type="text" placeholder="current-user-name" value={profile.username}
-                            onChange={(event) => {
-                              const newProfile = {
-                              ...profile, username: event.target.value,
-                              };
-                              setProfile(newProfile);
-                            }}/>
-                    </div>
-                </div>
-                <div className="row mt-3">
-                    <div className="col-4">
-                        <label>Change Password</label>
-                    </div>
-                    <div className="col-8">
-                        <input className="form-control" type="password" placeholder="current-password" value={profile.password}
-                            onChange={(event) => {
-                              const newProfile = {
-                              ...profile, password: event.target.value,
-                              };
-                              setProfile(newProfile);
-                            }}/>
-                    </div>
-                </div>
-                <div className="mt-2">
-                    <Link to="/zinema/settings" className="btn btn-primary mt-2 mb-2 me-2" type="button">Cancel</Link>
-                    <Link to="/zinema/settings" className="btn btn-primary mt-2 mb-2 me-2" type="button" onClick={save}>Save</Link>
-                </div>
+  const { currentUser } = useSelector((state) => state.user);
+  const [profile, setProfile] = useState(currentUser || {}); // Set default empty object if currentUser is null
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const save = () => {
+    dispatch(updateUserThunk(profile));
+  };
+  useEffect(() => {
+    async function fetchData() {
+      const { payload } = await dispatch(profileThunk());
+      setProfile(payload);
+    }
+    fetchData();
+  }, [dispatch]);
+
+  return (
+    <div className="container-xl px-4 mt-4">
+      <div>
+        <nav className="nav nav-tabs mb-2">
+          <Link className="nav-link ms-0" to="/zinema/settings-main">
+            Profile
+          </Link>
+          <Link className="nav-link" to="/zinema/billing">
+            Billing
+          </Link>
+          <Link className="nav-link active" to="/zinema/security">
+            Security
+          </Link>
+        </nav>
+        <div style={{ color: "black" }}>
+          <h1>Security Page</h1>
+
+          <div className="row">
+            <div className="col-4">
+              <label>Change Username</label>
             </div>
+            <div className="col-8">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="current-user-name"
+                value={profile.username || ""} // Check for username existence
+                onChange={(event) => {
+                  const newProfile = {
+                    ...profile,
+                    username: event.target.value,
+                  };
+                  setProfile(newProfile);
+                }}
+              />
             </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-4">
+              <label>Change Password</label>
+            </div>
+            <div className="col-8">
+              <input
+                className="form-control"
+                type="password"
+                placeholder="current-password"
+                value={profile.password || ""} // Check for password existence
+                onChange={(event) => {
+                  const newProfile = {
+                    ...profile,
+                    password: event.target.value,
+                  };
+                  setProfile(newProfile);
+                }}
+              />
+            </div>
+          </div>
+          <div className="mt-2">
+            <button
+              className="btn btn-primary mt-2 mb-2 me-2"
+              onClick={() => navigate("/zinema/profile")} // Wrap navigate call in a function
+            >
+              Cancel
+            </button>
+            <button className="btn btn-primary mt-2 mb-2 me-2" onClick={save}>
+              Save
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Security;
--- src/zinema/settings/settings.js
+++ src/zinema/settings/settings.js
@@ -1,28 +1,29 @@
 import "./index1.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { profileThunk, updateUserThunk } from "../services/auth-thunks";
 import { useSelector, useDispatch } from "react-redux";
 import React, { useState, useEffect } from "react";
 
 function SettingsMain() {
-    const { currentUser } = useSelector((state) => state.user);
-    const [profile, setProfile] = useState(currentUser);
-    const dispatch = useDispatch();
-    const save = () => { dispatch(updateUserThunk(profile)); };
-    useEffect(() => {
-        async function fetchData() {	
-            const { payload } = await dispatch(profileThunk());	
-            setProfile(payload);	
-          }	
-          fetchData();	
-        }, [dispatch]);
+  const { currentUser } = useSelector((state) => state.user);
+  const [profile, setProfile] = useState(currentUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const save = () => { dispatch(updateUserThunk(profile)); };
+  useEffect(() => {
+    const fetchData = async () => {
+      const { payload } = await dispatch(profileThunk());
+      setProfile(payload || {});
+    };
+    fetchData();
+  }, [dispatch]);
     return(
       <>
         <div className="container-xl px-4 mt-4">
           <nav className="nav nav-tabs mb-2">
             <Link className="nav-link active ms-0" to="/zinema/settings-main">Profile</Link>
             <Link className="nav-link" to="/zinema/billing">Billing</Link>
-            <Link className="nav-link" to="/zinema/security">Security</Link>
+            <a className="nav-link" href="/zinema/security">Security</a>
           </nav>
           <div className="row">
             <div className="col-md-4">
@@ -30,7 +31,7 @@ function SettingsMain() {
                 <div className="card-header">Profile Picture</div>
                 <div className="card-body d-flex flex-column align-items-center justify-content-center">
                   <img className="img-account-profile rounded-circle mb-2" src={profile.profilePicture} alt=""/>
-                  <Link className="btn btn-primary mt-2" to="/zinema/image-selection" type="button">Upload new image</Link>
+                  <button className="btn btn-primary mt-2" onClick={() => navigate("/zinema/image-selection")}>Upload new image</button>
                 </div>
               </div>
             </div>
@@ -111,7 +112,7 @@ function SettingsMain() {
                         </div>
                     </div>
                   </form>
-                  <Link to="/zinema/settings" className="btn btn-primary" type="button" onClick={save}>Save changes</Link>
+                  <button className="btn btn-primary" type="button" onClick={save}>Save changes</button>
                 </div>
               </div>
             </div>
